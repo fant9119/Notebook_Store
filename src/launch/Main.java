347848d0a1dd9b4edf619cae11c31a0627355fed
rk <fant9119@gmail.com>
2015-07-16 14:58:18 +0300

    little change)

--- src/launch/Main.java
+++ src/launch/Main.java
@@ -5,8 +5,8 @@ import view.Menu;
 public class Main {
 
 	public static void main(String[] args) {
-		Menu menu = new Menu();
-        menu.main();
+		Menu menu = new Menu();	
+		menu.main();
 	}
 
 }
